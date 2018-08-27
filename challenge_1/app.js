6dcaffea562833523429dfd2168904d9a30d8a4a
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-27 11:29:50 -0700

    Text at the top of the board will now display current player's turn.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,20 +1,23 @@
 var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 console.log("Game Start!");
+var topBanner = document.getElementById("top banner");
 var nextClick = "X";
 var handleSquareClick = event => {
   event.toElement.innerText = nextClick;
   if (nextClick === "X") {
     nextClick = "O";
+    topBanner.innerText = "Player O's Turn!";
   } else {
     nextClick = "X";
+    topBanner.innerText = "Player X's Turn!";
   }
 };
+
 var squareElements = document.getElementsByClassName("square");
 for (let i = 0; i < squareElements.length; i++) {
   squareElements[i].onclick = handleSquareClick;
 }
-var oneOne = document.getElementById("1,1");
-// oneOne.onclick = handleSquareClick;
+
 var resetElement = document.getElementById("reset");
 resetElement.addEventListener("click", function(event) {
   board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
