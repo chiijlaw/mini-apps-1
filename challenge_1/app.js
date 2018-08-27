ea9bcd2b536b205b690f6efb2a941843583f508a
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-27 11:10:00 -0700

    Set up board and reset button. Button does not yet reset the board.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,11 @@
-
-
+var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+console.log("Game Start!");
+var nextClick = "X";
+var squareElements = document.getElementsByClassName("square");
+// squareElements.onclick = alert("clicked it");
+var resetElement = document.getElementById("reset");
+resetElement.addEventListener("click", function(event) {
+  board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  console.log("pressed it");
+  squareElements.innerHTML = "asdf";
+});
