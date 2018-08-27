6aa98226835103a68429fb3bce0235441d58a5a9
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-27 12:19:51 -0700

    Reset button now clears both boards and reset's the top banner.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,15 +3,17 @@ console.log("Game Start!");
 var topBanner = document.getElementById("top banner");
 var nextClick = "X";
 var boardValue = 1;
-var extractBoardSquareFromId = function(id) {
-  id.split(",");
-};
+
 var updateBoardOnClick = function(node) {
   var coordinates = node.toElement.id.split(",");
   var row = parseInt(coordinates[0] - 1);
   var column = parseInt(coordinates[1] - 1);
   board[row][column] = boardValue;
 };
+
+// var checkForWin = function() {
+//     if ()
+// };
 
 var handleSquareClick = event => {
   updateBoardOnClick(event);
@@ -35,6 +37,8 @@ for (let i = 0; i < squareElements.length; i++) {
 var resetElement = document.getElementById("reset");
 resetElement.addEventListener("click", function(event) {
   board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-  console.log("pressed it");
-  squareElements.innerTEXT = "asdf";
+  for (let i = 0; i < squareElements.length; i++) {
+    squareElements[i].innerText = "[ ]";
+  }
+  topBanner = "Player X Starts First!";
 });
