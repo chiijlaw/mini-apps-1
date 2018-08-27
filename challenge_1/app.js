10bc923c0227a8dce826df622ea166de28853629
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-27 11:56:07 -0700

    Clicks will now change the hidden board state.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,13 +2,23 @@ var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 console.log("Game Start!");
 var topBanner = document.getElementById("top banner");
 var nextClick = "X";
+var boardValue = 1;
+var extractBoardSquareFromId = function(id) {
+  id.split(",");
+};
 var handleSquareClick = event => {
+  var coordinates = event.toElement.id.split(",");
+  var row = parseInt(coordinates[0] - 1);
+  var column = parseInt(coordinates[1] - 1);
   event.toElement.innerText = nextClick;
+  board[row][column] = boardValue;
   if (nextClick === "X") {
     nextClick = "O";
+    boardValue = -1;
     topBanner.innerText = "Player O's Turn!";
   } else {
     nextClick = "X";
+    boardValue = 1;
     topBanner.innerText = "Player X's Turn!";
   }
 };
