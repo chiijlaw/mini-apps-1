71896f988ae24df8e927c4820f220e0eabffd982
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-27 11:20:26 -0700

    handleClick event will alternate between changing the square's inner text to X or O.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,21 @@
 var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 console.log("Game Start!");
 var nextClick = "X";
+var handleSquareClick = event => {
+  event.toElement.innerText = nextClick;
+  if (nextClick === "X") {
+    nextClick = "O";
+  } else {
+    nextClick = "X";
+  }
+};
 var squareElements = document.getElementsByClassName("square");
-// squareElements.onclick = alert("clicked it");
+squareElements.onclick = event => alert("clicked it");
+var oneOne = document.getElementById("1,1");
+oneOne.onclick = handleSquareClick;
 var resetElement = document.getElementById("reset");
 resetElement.addEventListener("click", function(event) {
   board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
   console.log("pressed it");
-  squareElements.innerHTML = "asdf";
+  squareElements.innerTEXT = "asdf";
 });
