6db446ee973b7e58d34b17d64c66e1876040fa46
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-28 14:20:13 -0700

    Set up express server and serve up client. Client will accept JSON and POST it to the server. Server currently does not send back CMV-formatted data. The client will take that data and display it on the page.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
-
-
+$("form").on("submit", function(event) {
+  event.preventDefault();
+  var formElement = document.getElementById("formId");
+  var FD = new FormData(formElement);
+  var dataObj = {};
+  for (var value of FD.values()) {
+    dataObj.data = value;
+  }
+  $.post("/", dataObj.data).done(function(data) {
+    console.log(data);
+    $("#result")
+      .empty()
+      .append(`<p>${JSON.stringify(data)}</p>`);
+  });
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,27 @@
+const express = require("express");
+const app = express();
 
+app.use(express.static("client"));
 
+app.get("/", function(req, res) {
+  console.log("serving GET");
+  res.status(200);
+  res.send("hello world!");
+});
+
+app.post("/", function(req, res) {
+  console.log("serving POST");
+  var body = [];
+  req
+    .on("data", function(chunk) {
+      body.push(chunk);
+    })
+    .on("end", function() {
+      body = Buffer.concat(body).toString();
+
+      res.status(201);
+      res.send(JSON.parse(body));
+    });
+});
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
