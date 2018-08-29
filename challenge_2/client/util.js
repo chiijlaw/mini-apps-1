b6dac8d1faa3963471313f02a6c6c4343f11ae37
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-28 18:28:39 -0700

    Fixed typo. CMV is CSV

--- challenge_2/client/util.js
+++ challenge_2/client/util.js
@@ -23,25 +23,25 @@ exports.formatReport = function(obj) {
     delete child.children;
   });
 
-  // extract the keys from the first report for CMV-format
+  // extract the keys from the first report for CSV-format
   var topLineOfReport = Object.keys(reports[0]);
 
-  // extract the values from each report for CMV-format
+  // extract the values from each report for CSV-format
   var reportValues = [];
   reports.forEach(function(report) {
     var bucket = Object.values(report);
     reportValues.push(bucket);
   });
 
-  // assemble top line of CMV-formated report from keys
-  var CMVReport = "<tr><td>";
-  CMVReport = CMVReport.concat(topLineOfReport.join("</td><td>"));
-  CMVReport = CMVReport.concat("</td></tr>");
-  // assemble each row of CMV-formated report from values
+  // assemble top line of CSV-formated report from keys
+  var CSVReport = "<tr><td>";
+  CSVReport = CSVReport.concat(topLineOfReport.join("</td><td>"));
+  CSVReport = CSVReport.concat("</td></tr>");
+  // assemble each row of CSV-formated report from values
   reportValues.forEach(function(reportArray) {
-    CMVReport = CMVReport = CMVReport.concat("<tr><td>");
-    CMVReport = CMVReport.concat(reportArray.join("</td><td>"));
-    CMVReport = CMVReport = CMVReport.concat("</td></tr>");
+    CSVReport = CSVReport = CSVReport.concat("<tr><td>");
+    CSVReport = CSVReport.concat(reportArray.join("</td><td>"));
+    CSVReport = CSVReport = CSVReport.concat("</td></tr>");
   });
-  return CMVReport;
+  return CSVReport;
 };
