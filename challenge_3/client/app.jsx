79817e03d0b25d06ebb93ea6b6369f2b998fd424
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-29 14:54:23 -0700

    Each form component now has a button called next. Next will hide the current form and reveal the next form. The Summary page contains a purchase button that will send the user back to the homepage.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -16,7 +16,32 @@ class App extends React.Component {
     });
   }
 
-  handleNextClick(e) {}
+  handleNextClick() {
+    if (this.state.form1Display === "block") {
+      this.setState({
+        form1Display: "none",
+        form2Display: "block"
+      });
+    } else if (this.state.form2Display === "block") {
+      this.setState({
+        form2Display: "none",
+        form3Display: "block"
+      });
+    } else if (this.state.form3Display === "block") {
+      this.setState({
+        form3Display: "none",
+        summaryDisplay: "block"
+      });
+    }
+  }
+
+  handleSummaryClick() {
+    //send information to database
+    this.setState({
+      summaryDisplay: "none",
+      homePageDisplay: "block"
+    });
+  }
 
   render() {
     return (
@@ -26,10 +51,22 @@ class App extends React.Component {
           display={this.state.homePageDisplay}
           handleCheckoutClick={this.handleCheckoutClick.bind(this)}
         />
-        <Form1 display={this.state.form1Display} />
-        <Form2 display={this.state.form2Display} />
-        <Form3 display={this.state.form3Display} />
-        <Summary display={this.state.summaryDisplay} />
+        <Form1
+          display={this.state.form1Display}
+          handleNextClick={this.handleNextClick.bind(this)}
+        />
+        <Form2
+          display={this.state.form2Display}
+          handleNextClick={this.handleNextClick.bind(this)}
+        />
+        <Form3
+          display={this.state.form3Display}
+          handleNextClick={this.handleNextClick.bind(this)}
+        />
+        <Summary
+          display={this.state.summaryDisplay}
+          handleSummaryClick={this.handleSummaryClick.bind(this)}
+        />
       </div>
     );
   }
@@ -45,18 +82,39 @@ var Homepage = props => (
 var Form1 = props => (
   <div style={{ display: props.display }}>
     Form1!!
-    <button
-      name="submit"
-      onClick={event => {
-        console.log(event.target);
-      }}
-    >
+    <form>
+      <p>
+        <input type="text" />
+      </p>
+    </form>
+    <button name="submit" onClick={props.handleNextClick}>
       Next
     </button>
   </div>
 );
-var Form2 = props => <div style={{ display: props.display }}>Form2!!</div>;
-var Form3 = props => <div style={{ display: props.display }}>Form3!!</div>;
-var Summary = props => <div style={{ display: props.display }}>Summary!!</div>;
+var Form2 = props => (
+  <div style={{ display: props.display }}>
+    Form2!!
+    <button name="submit" onClick={props.handleNextClick}>
+      Next
+    </button>
+  </div>
+);
+var Form3 = props => (
+  <div style={{ display: props.display }}>
+    Form3!!
+    <button name="submit" onClick={props.handleNextClick}>
+      Next
+    </button>
+  </div>
+);
+var Summary = props => (
+  <div style={{ display: props.display }}>
+    Summary!!
+    <button name="submit" onClick={props.handleSummaryClick}>
+      Purchase
+    </button>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
