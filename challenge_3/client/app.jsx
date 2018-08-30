2a6c2841cefdaa2b695ff19635760637b8a0527c
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-29 17:57:59 -0700

    Form components are ready to send requests to the server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -6,8 +6,7 @@ class App extends React.Component {
       form1Display: "none",
       form2Display: "none",
       form3Display: "none",
-      summaryDisplay: "none",
-      formData: {}
+      summaryDisplay: "none"
     };
   }
   handleCheckoutClick() {
@@ -17,27 +16,38 @@ class App extends React.Component {
     });
   }
 
-  handleNextClick() {
+  handleNextClick(myData) {
     if (this.state.form1Display === "block") {
       this.setState({
         form1Display: "none",
         form2Display: "block"
       });
+      axios.post("/", myData);
     } else if (this.state.form2Display === "block") {
       this.setState({
         form2Display: "none",
         form3Display: "block"
       });
+      axios.put("/", myData);
     } else if (this.state.form3Display === "block") {
       this.setState({
         form3Display: "none",
         summaryDisplay: "block"
       });
+      axios.put("/", myData);
     }
   }
 
+  handleFormDataUpdate(someKey, someValue, formData) {
+    var obj = {};
+    obj[someKey] = someValue;
+    this.setState({
+      formData: Object.assign(obj, formData)
+    });
+  }
+
   handleSummaryClick() {
-    //TODO send information to server
+    axios.put("/", { complete: true });
     this.setState({
       summaryDisplay: "none",
       homePageDisplay: "block"
@@ -81,73 +91,164 @@ var Homepage = props => (
   </div>
 );
 
-var Form1 = props => (
-  <div style={{ display: props.display }}>
-    Form1!!
-    <form>
-      <p>
-        Name
-        <input type="text" name="name" />
-      </p>
-      <p>
-        Email
-        <input type="text" name="email" />
-      </p>
-      <p>
-        Password
-        <input type="text" name="password" />
-      </p>
-    </form>
-    <button name="submit" onClick={props.handleNextClick}>
-      Next
-    </button>
-  </div>
-);
+class Form1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div style={{ display: this.props.display }}>
+        Form1!!
+        <form>
+          <p>
+            Name
+            <input
+              type="text"
+              name="name"
+              onChange={e => this.setState({ name: e.target.value })}
+            />
+          </p>
+          <p>
+            Email
+            <input
+              type="text"
+              name="email"
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+          </p>
+          <p>
+            Password
+            <input
+              type="text"
+              name="password"
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+          </p>
+        </form>
+        <button
+          name="submit"
+          onClick={() => {
+            this.props.handleNextClick(this.state);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
 
-var Form2 = props => (
-  <div style={{ display: props.display }}>
-    Form2!!
-    <form>
-      <p>
-        Address
-        <input type="text" name="addressLine1" />
-        <input type="text" name="addressLine2" />
-      </p>
-      <p>
-        <input type="text" name="city" />
-        <input type="text" name="state" />
-        <input type="text" name="zipCode" />
-      </p>
-      <p>
-        Phone Number
-        <input type="text" name="phoneNumber" />
-      </p>
-    </form>
-    <button name="submit" onClick={props.handleNextClick}>
-      Next
-    </button>
-  </div>
-);
+class Form2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div style={{ display: this.props.display }}>
+        Form2!!
+        <form>
+          <p>
+            Address
+            <input
+              type="text"
+              name="addressLine1"
+              onChange={e => this.setState({ addressLine1: e.target.value })}
+            />
+            <input
+              type="text"
+              name="addressLine2"
+              onChange={e => this.setState({ addressLine2: e.target.value })}
+            />
+          </p>
+          <p>
+            <input
+              type="text"
+              name="city"
+              onChange={e => this.setState({ city: e.target.value })}
+            />
+            <input
+              type="text"
+              name="state"
+              onChange={e => this.setState({ state: e.target.value })}
+            />
+            <input
+              type="text"
+              name="zipCode"
+              onChange={e => this.setState({ zipCode: e.target.value })}
+            />
+          </p>
+          <p>
+            Phone Number
+            <input
+              type="text"
+              name="phoneNumber"
+              onChange={e => this.setState({ phoneNumber: e.target.value })}
+            />
+          </p>
+        </form>
+        <button
+          name="submit"
+          onClick={() => {
+            this.props.handleNextClick(this.state);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
 
-var Form3 = props => (
-  <div style={{ display: props.display }}>
-    Form3!!
-    <form>
-      <p>
-        Credit Card
-        <input type="text" name="creditCard" />
-      </p>
-      <p>
-        <input type="text" name="expiryDate" />
-        <input type="text" name="CVV" />
-        <input type="text" name="billingZip" />
-      </p>
-    </form>
-    <button name="submit" onClick={props.handleNextClick}>
-      Next
-    </button>
-  </div>
-);
+class Form3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div style={{ display: this.props.display }}>
+        Form3!!
+        <form>
+          <p>
+            Credit Card
+            <input
+              type="text"
+              name="creditCard"
+              onChange={e => this.setState({ creditCard: e.target.value })}
+            />
+          </p>
+          <p>
+            <input
+              type="text"
+              name="expiryDate"
+              onChange={e => this.setState({ expiryDate: e.target.value })}
+            />
+            <input
+              type="text"
+              name="CVV"
+              onChange={e => this.setState({ CVV: e.target.value })}
+            />
+            <input
+              type="text"
+              name="billingZip"
+              onChange={e => this.setState({ billingZip: e.target.value })}
+            />
+          </p>
+        </form>
+        <button
+          name="submit"
+          onClick={() => {
+            this.props.handleNextClick(this.state);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
 
 var Summary = props => (
   <div style={{ display: props.display }}>
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = "mongodb://localhost:27017";
+
+dbName = "customers";
+
+MongoClient.connect(
+  url,
+  function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    client.close();
+  }
+);
 
 app.use(express.static("public"));
 
@@ -19,8 +37,24 @@ app.post("/", (req, res) => {
     .on("end", () => {
       body = Buffer.concat(body).toString();
       //TODO send body onto database
+      console.log(body, "----body----");
       res.status(201);
       res.send("Posted info");
+    });
+});
+
+app.put("/", (req, res) => {
+  console.log("serving PUT");
+  let body = [];
+  req
+    .on("data", chunk => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      //TODO send body into database
+      res.status(201);
+      res.send("Updates database");
     });
 });
 
