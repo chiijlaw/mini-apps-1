4d6855853af7e0a75feb57732dd88e8882d0f4a6
Chiijlaw <chiijlaw@berkeley.edu>
2018-08-30 20:15:20 -0700

    Installed mocha. No tests have been implemented yet.

---
+++ challenge_4/spec/test.js
@@ -0,0 +1,7 @@
+var assert = require("assert");
+
+describe("Should check for win", function() {
+  describe("it should alert when there are four consecutive pieces in a Row", function() {
+    //TODO write test
+  });
+});
